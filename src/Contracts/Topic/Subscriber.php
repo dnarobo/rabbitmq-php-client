9f4cbc0c1a01a442b21a9ac4bee8600f8e263142
garam park <me@garam.xyz>
2018-07-19 15:44:43 +0900

    no message

--- src/Contracts/Topic/Subscriber.php
+++ src/Contracts/Topic/Subscriber.php
@@ -5,6 +5,7 @@ namespace Dna\RabbitMq\Contracts\Topic;
 use PhpAmqpLib\Message\AMQPMessage;
 use PhpAmqpLib\Connection\AbstractConnection;
 
+use Dna\RabbitMq\Contracts\Client;
 abstract class Subscriber extends Client
 {
     /**
